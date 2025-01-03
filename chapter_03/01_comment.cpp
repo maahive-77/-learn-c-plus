234e59f5bdc578e80dcbcd4ae976ae51cd819a6e
maahi_ve <154302147+maahive-77@users.noreply.github.com>
2025-01-03 23:46:50 +0530

    source code
    C++ Variable
    
    Variables in C++ is a name given to a memory location. It is the basic unit of storage in a program. 
    
    The value stored in a variable can be changed during program execution.
    A variable is only a name given to a memory location, all the operations done on the variable effects that memory location.
    In C++, all the variables must be declared before use.

---
+++ chapter_03/01_comment.cpp
@@ -0,0 +1,15 @@
+/* C++ program to illustrate 
+use of 
+multi-line comment */
+#include <iostream>
+int main()
+{
+    // Single line Welcome user comment
+    /* Multi-line Welcome user comment 
+    written to demonstrate comments
+    in C/C++ */
+    std::cout << "Welcome to GeeksforGeeks";
+    return 0;
+}
+
+// This code is contributed by sarajadhav12052009
